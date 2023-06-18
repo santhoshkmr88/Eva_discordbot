1d731d2ccfb7e8a3ba69b6fc2107d2ace33d88a0
santhoshkmr88 <santhoshkmr88@yahoo.com>
2023-06-18 18:45:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+<!-- GETTING STARTED -->
+## Getting Started
+
+This is an initial draft of discord bot with limited commands.
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [x] Add hello
+- [x] Add roll
+- [ ] Improve help
+- [ ] Improve ping
+- [ ] Add Multi-language Interpreter
+    - [ ] Japanese
+    - [ ] French
+
+More commands and instructions are on the build.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
---
+++ eva_discordbot.py
@@ -0,0 +1,75 @@
+import discord
+import random
+import configparser
+
+client = discord.Client(intents=discord.Intents.default())
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+bot_token = config.get('discord', 'bot_token')
+
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+
+@client.event
+async def on_message(message):
+    # Check if the message was sent by the bot itself.
+    if message.author.bot:
+        return
+
+    # Get the command and arguments from the message.
+    parts = message.content.split()
+    command = parts[1]
+    args = parts[1:]
+
+    # Check if the command is a known command.
+    if command in commands:
+        # Run the command.
+        await commands[command](message, args)
+    else:
+        # The command is not known, so respond with an error message.
+        await message.reply("Hi! I'm Eva. Sorry, I don't know that command. Type help to view all commands.")
+
+# A function that responds to the "ping" command.
+async def hello(message, args):
+    await message.reply("Hi! I'm Eva.")
+
+# A function that responds to the "ping" command.
+async def ping(message, args):
+    await message.reply("Pong!")
+
+# A function that responds to the "help" command.
+async def help(message, args):
+    await message.reply("Here is a list of commands:")
+    for command in commands:
+        await message.reply(f"* {command}")
+
+# A function that responds to the "roll" command.
+async def roll(message, args):
+    if not args:
+        await message.reply("Please provide the number of sides for the dice.")
+        return
+
+    # Get the number of sides from the message.
+    try:
+        sides = int(args[1])
+    except ValueError:
+        await message.reply("Invalid number of sides. Please provide a valid integer.")
+        return
+
+    # Roll the die.
+    result = random.randint(1, sides)
+
+    # Respond with the result.
+    await message.reply(f"You rolled a {result}.")
+
+# A dictionary of commands.
+commands = {
+    "hello": hello,
+    "ping": ping,
+    "help": help,
+    "roll": roll,
+}
+
+client.run(bot_token)
